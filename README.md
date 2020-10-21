5e2481b9252b6029e6a8f8454ee61a3489523750
JWahyu <71404267+JWahyu@users.noreply.github.com>
2020-10-21 09:59:22 +0200

    Eerste versie van het spel Roulette.

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # Amerikaanse Roulette
 
 Amerikaanse Roulette is een spel dat je kunt spelen bij Holland Roulette. Als speler probeer jij te voorspellen welk nummer er in de cilinder valt. Er zijn in totaal 37 nummers: 0 t/m 36. Er zijn verschillende mogelijkheden om de uitkomst te voorspellen. Een variant is dat je voorspelt dat de uitkomst een even of een oneven getal is. Als jouw voorspelling uitkomt, is je winst 1x je inzet en krijg je ook je inzet terug. Komt jouw voorspelling niet uit, dan verlies je je inzet. In het geval dat het getal 0 valt, verlies je ook inzet.
-- schrjif een programma dat Amerikaanse Roulette speelt. D.w.z. dat je programma dus willekeurig een getal 0 t/m 36 moet genereren.
-- de inzet per ronde is minimaal 5 Euro of een veelvoud van 5. Je krijgt een startbedrag van 100 Euro. Schrijf een programma dat 10 rondes Roulette speelt met:
-    - een inzet van 5 euro per keer 
-    - een willekeurige voorspelling doet op een even of oneven getal van 1 t/m 36 (dus geen 0)
-    - bij winst 5 euro optelt bij het bedrag
-    - bij verlies 5 euro aftrekt van het bedrag
+- DONE: schrjif een programma dat Amerikaanse Roulette speelt. D.w.z. dat je programma dus willekeurig een getal 0 t/m 36 moet genereren.
+- DONE: de inzet per ronde is minimaal 5 Euro of een veelvoud van 5. Je krijgt een startbedrag van 100 Euro. Schrijf een programma dat 10 rondes Roulette speelt met:
+    - DONE: een inzet van 5 euro per keer 
+    - DONE: een willekeurige voorspelling doet op een even of oneven getal van 1 t/m 36 (dus geen 0)
+    - DONE: bij winst 5 euro optelt bij het bedrag
+    - DONE: bij verlies 5 euro aftrekt van het bedrag
 - laat bij iedere ronde zien:
-    - wat de voorspelling is
-    - wat de uitslag is
-    - of er gewonnen is
-    - wat het bedrag is na de speelronde
+    - DONE: wat de voorspelling is
+    - DONE: wat de uitslag is
+    - DONE: of er gewonnen is
+    - DONE: wat het bedrag is na de speelronde
--- src/Roulette.java
+++ src/Roulette.java
@@ -1,6 +1,74 @@
-import java.util.Random;
+public class Roulette extends RoulletteOnderdelen {
 
-public class Roulette {
     public static void main(String[] args) {
-    }
-}
+
+        //  Initiatie spel: Amerikaanse Roulette.
+        System.out.printf("%n―――――――――――――――――――――――――――――――――――――――――%n");
+        System.out.print("♤♥♣♦ Welkom bij het spel Amerikaanse Roulette in ons casino! ♦♣♥♤");
+        System.out.printf("%n―――――――――――――――――――――――――――――――――――――――――%n");
+
+        // Invoer: nieuw spel of stop programma.
+        while (!checkNieuwSpel) {
+            System.out.printf("%nWil je een nieuw potje starten?%n%nDruk 'J/j' for ja of 'N/n' for nee om te stoppen en bevestig vervolgens je antwoord met 'ENTER'-toets: ");
+            var invoerExitGame = invoer.nextLine();
+
+            // Optie Nieuw Spel: invoer 'J/j' nieuw / herhaal programma.
+            if (invoerExitGame.equalsIgnoreCase("j")) {
+
+                // Parent class Roulette.
+                Roulette roulette = new Roulette(); // Constructor wordt hier gerund.
+
+                System.out.printf("%n――――――――――――――%n");
+                System.out.print("♤♥♣♦  NIEUW POTJE ♦♣♥♤");
+                System.out.printf("%n――――――――――――――%n");
+
+                System.out.printf("%nGraag je naam invullen en bevestig vervolgens je keuze met 'ENTER'-toets: ");
+                String spelerNaam = invoer.nextLine();
+
+                System.out.printf("" +
+                        "%nAangenaam " + spelerNaam + "! Je speelt 10 rondes roulette met een begin-inzet van: € " + inzetSpeler + ",-%n" +
+                        "Als je een ronde wint dan ontvang je het dubbele van jouw ronde-inzet!%n%n" +
+
+                        "Je kan per ronde op twee manier wedden:%n" +
+                        "1. Veilig: wedden op even of oneven.%n" +
+                        "2. Gewaagd: wedden op een nummer.%n%n" +
+                        "Heb je de ronde verloren of bij uitkomst 0 dan wint de bank en verlies je jouw ronde-inzet!%n%n" +
+                        "We gaan beginnen! Succes!%n");
+
+                // Invoer: Druk op 'ENTER'-toets om door te gaan.
+                invoerEnter();
+
+                // 10 Rondes Roulette.
+                for (rondeNummer = 1; rondeNummer <= 10; rondeNummer++) {
+                    ronde();
+                    // Invoer: Druk op 'ENTER'-toets om door te gaan.
+                    invoerEnter();
+                } // Einde 10 Rondes Roulette.
+
+                System.out.printf("%n――――――――――――――%n");
+                System.out.print("♤♥♣♦  EINDE POTJE ♦♣♥♤");
+                System.out.printf("%n――――――――――――――%n");
+
+                reset();
+            } // Einde Optie Nieuwe Spel: invoer 'J/j' nieuw spel.
+
+
+            else if (invoerExitGame.equalsIgnoreCase("n")) // Optie Nieuwe Spel: invoer 'N/n' einde programma.
+            {
+                System.out.printf("%n――――――――――――――――――――――――――――――――――%n");
+                System.out.print("Dat was het dan. Dank voor het spelen en tot ziens! :)");
+                System.out.printf("%n――――――――――――――――――――――――――――――――――%n");
+                checkNieuwSpel = true;
+
+
+            } // Einde Optie Nieuwe Spel: invoer 'N/n' einde programma.
+
+            else // Optie Nieuwe Spel: ongeldige invoer.
+            {
+                geenGeldigeInvoer();
+                checkNieuwSpel = false;
+            } // Einde Optie Nieuwe Spel: ongeldige invoer.
+
+        } // Einde Nieuwe Spel.
+    } // Einde public static void main(String[] args)
+} // Einde public class AmerikaanseRoulette extends Roulette
